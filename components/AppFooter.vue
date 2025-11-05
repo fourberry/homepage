13a0cb3e5b185835dc58c87d62246140e20b21da
Jeong Haeseong <briskly0415@gmail.com>
2025-11-05 10:17:00 +0900

    푸터 수정

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,24 +1,30 @@
 <script setup lang="ts"></script>
 
 <template>
-    <footer class="footer">
-        <div class="container">
-            <p>&copy; 2025 FOURBERRY Inc. All rights reserved.</p>
-            <p>서울특별시 영등포구 양평로 22길 21 코오롱디지털타워 1409호</p>
+    <footer class="bg-accent text-white pb-8">
+        <div class="container-wide flex flex-col gap-2 text-center">
+            <div class="text-sm font-semibold">(주)포베리</div>
+            <div class="text-xs font-extralight">(07205) 서울특별시 영등포구 양평로 22길 21 1409호</div>
+            <div class="text-xs font-extralight flex justify-center gap-1">
+                <span>대표이사 : 우대식</span>
+                <span>|</span>
+                <span>사업자등록번호: 562-88-02654</span>
+            </div>
+            <div class="text-xs font-extralight flex justify-center gap-1">
+                <span>Tel: 010-2755-0650</span>
+                <span>|</span>
+                <span>Email: fourberry@fourberry.co.kr</span>
+            </div>
+            <div class="p-4 flex align-middle justify-center">
+                <div class="w-48">
+                    <img src="/images/logo_text_white_transparent.png" />
+                </div>
+            </div>
+            <!-- <p>&copy; 2025 FOURBERRY Inc. All rights reserved.</p>
+            <p>서울특별시 영등포구 양평로 22길 21 코오롱디지털타워 1409호</p> -->
         </div>
     </footer>
 </template>
 
 <style scoped>
-.footer {
-    background-color: #f8f9fa;
-    padding: 2rem 0;
-    text-align: center;
-    color: #6c757d;
-    border-top: 1px solid #eaeaea;
-}
-.container {
-    max-width: 1200px;
-    margin: 0 auto;
-}
 </style>
